94630c8cb1e83f16841093134ac97bfcb26c4ee5
Coderro01 <137710089+Coderro01@users.noreply.github.com>
2025-05-07 00:55:00 +0530

    Only motion left

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
+
 import { FaRegFileCode } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa6";
 import { ImCross } from "react-icons/im";
+import { motion } from "framer-motion";
 
-function Card({ data }) {
+function Card({ data , reference }) {
   return (
     <>
-      <div className="relative h-72 w-60 overflow-hidden rounded-[40px] bg-red-950 px-8 py-10">
+        <motion.div dragConstraint={ reference} className="flex-shring-0 relative h-72 w-60 overflow-hidden rounded-[40px] bg-red-950 px-8 py-10"
+      >
         <FaRegFileCode />
         <p className="mt-5 text-xl leading-tight">{data.detail}</p>
         {/*fetch from frgrd.jsx*/}
@@ -25,14 +28,17 @@ function Card({ data }) {
             </div>
           </div>
 
-          {data.tag.isOpen ? (
-            <div className="tag flex w-full items-center justify-center bg-green-600 py-4">
-              <h3 className="text-[17px] font-semibold">Dowload Now</h3>
+          {/*tagColor & tagTiltle*/}
+          {data.tag.isOpen && (
+            <div
+              className={`tag flex w-full items-center justify-center ${data.tag.tagColor === "blue" ? "bg-red-400" : "bg-yellow-500"} py-4`}
+            >
+              <h3 className="text-[17px] font-semibold">{data.tag.tagTitle}</h3>
             </div>
-          ) : null} {/*agr open hai ya dwnloadable hai toh show kro nhi toh kuch mt dhekhaao*/}
-          
+          )}
+          {/*agr open hai ya dwnloadable hai toh show kro nhi toh kuch mt dhekhaao*/}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
--- src/component/Frgrd.jsx
+++ src/component/Frgrd.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
+import { useRef } from "react";
 import Card from "./Card";
 
 function Frgrd() {
-  {
-    /*const icon, detail,closeorDownload , tag*/
-  }
+  const ref = useRef(null);
 
   const data = [
     {
-      detail:"This is Lorem text box and it has to be more responsive as if i write the text more then it will hide beside the download and file size.",
+      detail:
+        "This is Lorem text box and it has to be more responsive as if i write the text more then it will hide beside the download and file size.",
+      fileSize: "0.5mb",
+      close: "true",
+      tag: { isOpen: true, tagTitle: "Downloaded", tagColor: "red" },
+    },
+    {
+      detail:
+        "This is Lorem text box and it has to be more responsive as if i write the text more then it will hide beside the download and file size.",
+      fileSize: "0.5mb",
+      close: "true",
+      tag: { isOpen: true, tagTitle: "Downloaded", tagColor: "red" },
+    },
+    {
+      detail:
+        "This is Lorem text box and it has to be more responsive as if i write the text more then it will hide beside the download and file size.",
       fileSize: "0.5mb",
       close: "true",
       tag: { isOpen: true, tagTitle: "Downloaded", tagColor: "red" },
@@ -18,15 +32,12 @@ function Frgrd() {
 
   return (
     <>
-      <div className="fixed top-0 left-0 z-[3] h-full w-full">
-        {/* {data.map(items, index) = function () {
-          <Card data={items} />
-}} */}
-        {/* {data.map((items, index)=>(
-        <Card data={items} />
-        ))} */}
-        {data.map((items, index) => (
-          <Card data={items} />
+      <div
+        ref={ref}
+        className="fixed top-0 left-0 z-[3] flex h-full w-full flex-wrap gap-6 p-2"
+      >
+        {data.map((items) => (
+          <Card data={items} reference={ref} />
         ))}
       </div>
       {/*bcz of the above paarent div in app.jsx we cannot select the bkgrnd text content which is overlap by this*/}
